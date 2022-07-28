5f65b30a7c00e285264df9991e33402cee01b913
Zachary Smith <zach2993@gmail.com>
2022-07-28 14:17:27 -0500

    users calls are displaying on there logbook

---
+++ src/components/calls/NewCallForm.js
@@ -0,0 +1,98 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export const NewCallForm = () => {
+    /*
+        TODO: Add the correct default properties to the
+        initial state object
+    */
+    const [newCall, updateCallList] = useState({
+            zoneId: newCall.zoneId,
+            equipmentId: newCall.equipmentId,
+            issueDetailsId: newCall.issueDetailsId,
+            date: newCall.date,
+            startTime: newCall.startTime,
+            endTime: newCall.endTime,
+            totalAmountOfDowntime: newCall.totalAmountOfDowntime,
+    })
+    /*
+        TODO: Use the useNavigation() hook so you can redirect
+        the user to the ticket list
+    */
+    const navigate = useNavigate()
+    const localUser = localStorage.getItem("whatHappened_user")
+    const whatHappenedUserObject = JSON.parse(localUser)
+
+    const handleSaveButtonClick = (event) => {
+        event.preventDefault()
+
+        // TODO: Create the object to be saved to the API
+        const callToSendToAPI = {
+            userId: whatHappenedUserObject.id,
+            zoneId: newCall.zoneId,
+            equipmentId: newCall.equipmentId,
+            issueDetailsId: newCall.issueDetailsId,
+            date: newCall.date,
+            startTime: newCall.startTime,
+            endTime: newCall.endTime,
+            totalAmountOfDowntime: newCall.totalAmountOfDowntime,
+            descriptionOfIssue: "",
+            RepairMade: ""
+        }
+
+        // TODO: Perform the fetch() to POST the object to the API
+        return fetch(`http://localhost:8088/loggedCalls`, {
+            method: "POST",
+            headers: {
+                "content-Type" : "application/json"
+            },
+            body: JSON.stringify(callToSendToAPI)
+        })
+        .then(response => response.json())
+        .then(() => {
+            navigate("/")
+        })
+    }
+
+    return (
+        <form className="newCallForm">
+            <h2 className="newCallForm__title">New Call</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="description">Zone</label>
+                    <input
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        placeholder="Zone"
+                        value={newCall.zoneId}
+                        onChange={ 
+                            (evt) => {
+                                const copy = {...newCall}
+                                copy.description = evt.target.value
+                                updateCallList(copy)
+                            }
+                            } />
+                </div>
+            </fieldset>
+            {/* <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Emergency:</label>
+                    <input type="checkbox"
+                        value={ticket.emergency}
+                        onChange={
+                            (evt) => {
+                                const copy = {...ticket}
+                                copy.emergency = evt.target.checked
+                                update(copy)
+                            }} />
+                </div>
+            </fieldset> */}
+            <button 
+            onClick={(clickEvent) => handleSaveButtonClick(clickEvent)}
+            className="btn btn-primary">
+                Submit to Logbook
+            </button>
+        </form>
+    )
+}
